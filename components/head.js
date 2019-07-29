14437d9ea0e2d980476d38ed5fd56a4c5160aa60
jackyzm <jackyzm@foxmail.com>
2019-07-29 13:01:17 +0800

    update

--- components/head.js
+++ components/head.js
@@ -2,39 +2,26 @@ import React from 'react';
 import NextHead from 'next/head';
 import { string } from 'prop-types';
 
-const defaultDescription = '';
-const defaultOGURL = '';
-const defaultOGImage = '';
-
 const Head = (props) => {
     const {
-        title, description, url, ogImage
+        title = '', description = '', url = ''
     } = props;
     return (
         <NextHead>
             <meta charSet="UTF-8" />
-            <title>{ title || '' }</title>
             <meta
                 name="description"
-                content={ description || defaultDescription }
+                content={ description }
             />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
-            <link rel="apple-touch-icon" href="/static/touch-icon.png" />
-            <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
-            <link rel="icon" href="/static/favicon.ico" />
-            <meta property="og:url" content={ url || defaultOGURL } />
-            <meta property="og:title" content={ title || '' } />
+            <meta property="og:url" content={ url } />
+            <meta property="og:title" content={ title } />
             <meta
                 property="og:description"
-                content={ description || defaultDescription }
+                content={ description }
             />
-            <meta name="twitter:site" content={ url || defaultOGURL } />
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:image" content={ ogImage || defaultOGImage } />
-            <meta property="og:image" content={ ogImage || defaultOGImage } />
-            <meta property="og:image:width" content="1200" />
-            <meta property="og:image:height" content="630" />
+            <title>{ title }</title>
+            <link rel="icon" href="/static/favicon.ico" />
         </NextHead>
     );
 };
@@ -44,7 +31,6 @@ Head.propTypes = {
     title: string,
     description: string,
     url: string,
-    ogImage: string
 };
 
 export default Head;
